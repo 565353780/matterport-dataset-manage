5438883716ba55a87be569340d9f8e1322420efb
565353780 <565353780@qq.com>
2022-04-30 00:43:33 +0800

    update code format

--- get_object.py
+++ get_object.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from Method.MatterPortLoader import demo
+from Method.MatterPortLoader import DatasetLoader
 
 if __name__ == "__main__":
-    demo()
+    dataset_loader = DatasetLoader()
+    dataset_loader.run_demo()
 
